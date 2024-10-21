4366a9b8fb53cb3070be5b2e9a44190c2ef8c26e
Shubham Pawar <114252697+shubhampawar4841@users.noreply.github.com>
2024-10-21 21:53:18 +0530

    Update upload.js

--- server/middlewares/upload.js
+++ server/middlewares/upload.js
@@ -15,21 +15,3 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage})
 
 module.exports = upload
-
-/* 
-    * Middleware to uplad images via multer
-*/
-const multer = require("multer")
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, `./public/${req.body.category}`)
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname + "-" + Date.now() + ".jpg")
-    }
-})
-
-const upload = multer({ storage: storage})
-
-module.exports = upload
