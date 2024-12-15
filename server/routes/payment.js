5439b4bc9019df76d54341edb680881904c13370
Shubham Pawar <114252697+shubhampawar4841@users.noreply.github.com>
2024-12-15 22:37:16 +0530

    Update payment.js

--- server/routes/payment.js
+++ server/routes/payment.js
@@ -3,10 +3,6 @@ const router = express.Router()
 const cors = require("cors")
 const { initializePayment, verifyPayment } = require("../controllers/paymentController")
 
-// initialize payment endpoint
-router.post('/', cors(), initializePayment)
 
-// verfiy payment endpoint
-router.get('/verify/:id', cors(), verifyPayment)
 
 module.exports = router
